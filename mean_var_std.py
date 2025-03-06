74e6b744fb74139d79567801425bb3f0fe2f8a23
Maria Fernandez Alonso <maria.fernandez.alonso@outlook.es>
2025-03-06 17:53:01 +0100

    feat: add calculations for mean, variance, and standard deviation in 3x3 matrix

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,22 @@
 import numpy as np
 
 def calculate(list):
+    #Discard list with less than 9 elements
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    #Convert into 3x3 Numpy array
+    matrix = np.array(list).reshape(3, 3)
 
+    #Dictionary
+    calculations = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().tolist()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().tolist()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().tolist()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().tolist()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().tolist()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().tolist()]
+    }
 
 
 
